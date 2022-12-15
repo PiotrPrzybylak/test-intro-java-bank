0e04369db06caa4cb27355549623cd6d1ea3c1bd
Piotr Przybylak <piotr.przybylak@gmail.com>
2022-12-15 14:10:27 +0100

    Improve testing framework

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
         bank.deposit("PL123", 30);
         float result = bank.getAmount("PL123");
-        testCondition(result == 30);
+        testAmount(result, 30, 1, "Wrong amount returned");
 
         bank.deposit("PL123", 15);
         result = bank.getAmount("PL123");
@@ -17,15 +17,28 @@ public class Main {
 
         bank.deposit("PL456", 15);
         result = bank.getAmount("PL456");
-        testCondition(result == 15);
+        testAmount(result, 15, 3, "Wrong amount returned");
+
 
         result = bank.getAmount("PL123");
-        testCondition(result == 45);
+        testAmount(result, 45, 4, "Wrong amount returned");
+
+
 
     }
 
+    private static void testAmount(float actual, float expected, int testNumber, String message) {
+        if (actual != expected) {
+            System.out.println("Test failed!");
+            System.out.println("Test numer: " + testNumber);
+            System.out.println(message);
+            System.out.println("Excpected: " + expected + " but recieved: " + actual);
+            System.out.println();
+        }
+    }
+
     private static void testCondition(boolean condition) {
-        if (!condition){
+        if (!condition) {
             System.out.println("NOT OK");
         }
     }
