6a5e1d225da8d100552371772e705927ba3c51c4
Piotr Przybylak <piotr.przybylak@gmail.com>
2022-12-15 14:48:14 +0100

    Refactor tests

--- src/test/java/org/example/bank/BankTest.java
+++ src/test/java/org/example/bank/BankTest.java
@@ -8,29 +8,40 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BankTest {
 
-    @Test
-    void test() {
-        Bank bank = new Bank();
+    private Bank bank = new Bank();
 
+    @Test
+    public void shouldCreateAccount() {
         bank.createAccount("PL123");
-        assertEquals(0, bank.getAmount("PL123"), "Wrong amount returned");
+        assertEquals(0, bank.getAmount("PL123"));
+    }
+
+    @Test
+    public void shouldDeposit() {
+        bank.createAccount("PL123");
 
         bank.deposit("PL123", 30);
         float result = bank.getAmount("PL123");
-        assertEquals(30, result, "Wrong amount returned");
-
-        bank.deposit("PL123", 15);
-        result = bank.getAmount("PL123");
-        assertTrue(result == 45);
-
-        bank.createAccount("PL456");
-        bank.deposit("PL456", 15);
-        result = bank.getAmount("PL456");
-        assertEquals(15, result, "Wrong amount returned");
-
-
-        result = bank.getAmount("PL123");
-        assertEquals(45, result, "Wrong amount returned");
+        assertEquals(30, result);
     }
 
+    @Test
+    public void shouldDepositTwice() {
+        bank.createAccount("PL123");
+        bank.deposit("PL123", 30);
+        bank.deposit("PL123", 15);
+        float result = bank.getAmount("PL123");
+        assertEquals(45, result);
+    }
+
+
+    @Test
+    public void shouldDepositOnDifferentAccounts() {
+        bank.createAccount("PL123");
+        bank.createAccount("PL456");
+        bank.deposit("PL123", 11);
+        bank.deposit("PL456", 50);
+        assertEquals(11, bank.getAmount("PL123"));
+        assertEquals(50, bank.getAmount("PL456"));
+    }
 }
